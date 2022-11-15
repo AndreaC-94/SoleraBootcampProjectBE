0337d1537257a0011e0728f142d6f91fd1b3b737
Conor Fairclough <conorjack.fairclough@solera.com>
2022-11-15 16:30:20 +0100

    GroupDao and fixes

--- src/main/java/com/solera/group4/BackendProjectSolera/controllers/GroupController.java
+++ src/main/java/com/solera/group4/BackendProjectSolera/controllers/GroupController.java
@@ -1,8 +1,18 @@
 package com.solera.group4.BackendProjectSolera.controllers;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.solera.group4.BackendProjectSolera.entities.Group;
 
 @RestController
 public class GroupController {
+	
+	@RequestMapping("/courses")
+	public List<Group> retrieveAllCourses() {
+	}
 
 }
--- src/main/java/com/solera/group4/BackendProjectSolera/entities/Group.java
+++ src/main/java/com/solera/group4/BackendProjectSolera/entities/Group.java
@@ -1,6 +1,7 @@
 package com.solera.group4.BackendProjectSolera.entities;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
@@ -16,9 +17,55 @@ import lombok.Setter;
 @Getter
 public class Group {
 
-    
+    private Integer id;
 	private String name;
     private List<Assignment> assignmentList;
     private int points;
+    
+	public Group(String name, int id) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.assignmentList = new ArrayList<Assignment>();
+		this.points = 0;
+	}
+
+	@Override
+	public String toString() {
+		return "Group [id=" + id + ", name=" + name + ", assignmentList=" + assignmentList + ", points=" + points + "]";
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Assignment> getAssignmentList() {
+		return assignmentList;
+	}
+
+	public void setAssignmentList(List<Assignment> assignmentList) {
+		this.assignmentList = assignmentList;
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public void setPoints(int points) {
+		this.points = points;
+	}
+    
     
 }
