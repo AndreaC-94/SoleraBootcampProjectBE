6b412278b7450def4323c610bdedac58e002ee2d
Andrea Colucci <Andrea.Colucci@solera.com>
2022-11-15 16:38:18 +0100

    test Again 2

--- src/main/java/com/solera/group4/BackendProjectSolera/controllers/GroupController.java
+++ src/main/java/com/solera/group4/BackendProjectSolera/controllers/GroupController.java
@@ -1,4 +1,4 @@
-package com.solera.group4.mainFolder.controllers;
+package com.solera.group4.BackendProjectSolera.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
--- src/main/java/repositories/GroupRepository.java
+++ src/main/java/repositories/GroupRepository.java
@@ -3,7 +3,7 @@ package repositories;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.solera.group4.mainFolder.entities.Group;
+import com.solera.group4.BackendProjectSolera.entities.Group;
 
 @Repository
 public interface GroupRepository extends MongoRepository<Group, String>{
