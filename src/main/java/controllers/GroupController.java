0104fed97679d5b919ca124438318480d2b59cba
Andrea Colucci <Andrea.Colucci@solera.com>
2022-11-16 16:55:24 +0100

    API: getAllGroups, CreateGroup, DeleteGroup

--- src/main/java/controllers/GroupController.java
+++ src/main/java/controllers/GroupController.java
@@ -1,8 +1,19 @@
 package controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import entities.Group;
 import services.GroupServices;
 
 @RestController
@@ -10,4 +21,45 @@ public class GroupController {
     
     @Autowired
     GroupServices groupServices;
+
+    @GetMapping
+    @RequestMapping("/getAll")
+    public List<Group> getAllGroup(){
+        return groupServices.getAllGroup();
+    }
+
+    @PostMapping
+    @RequestMapping("/createGroup")
+    @Transactional
+    public ResponseEntity<String> createGroup(String group){
+        try{
+            //TODO Add validation
+            String message = groupServices.createGroup(group);
+            return new ResponseEntity(message, HttpStatus.CREATED);
+        }catch(Exception e){
+            //String errorMessage ="something went wrong";
+            return new ResponseEntity(e.getMessage(), HttpStatus.CONFLICT);
+        }
+    }
+
+    @DeleteMapping
+    @RequestMapping("/deleteGroup")
+    @Transactional
+    public ResponseEntity<String> deleteGroup(String groupName){
+        try{
+            //TODO Add validation
+            String message = groupServices.deleteGroup(groupName);
+            return new ResponseEntity(message, HttpStatus.OK);
+        }catch(Exception e){
+            return new ResponseEntity(e.getMessage(), HttpStatus.CONFLICT);
+        }
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity<String> editGroup(String groupNameNow, String groupNameNew) throws Exception{
+        //TODO
+        String message = groupServices.editGroup(groupNameNow, groupNameNew);
+        return null;
+    }
 }
--- src/main/java/services/GroupServices.java
+++ src/main/java/services/GroupServices.java
@@ -1,8 +1,13 @@
 package services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import entities.Group;
 import repositories.GroupRepository;
 
 @Service
@@ -10,5 +15,33 @@ public class GroupServices {
 
     @Autowired
     GroupRepository groupRepository;
+
+    public List<Group> getAllGroup() {
+        List<Group> groups = groupRepository.findAll();
+        return groups;
+    }
     
+    public String createGroup(String group) throws Exception {
+        if(groupRepository.existsById(group)) throw new Exception("This group name already exists, pick a different name please.");
+        else{
+            groupRepository.save(new Group (group));
+            return "A new group named " + group + " created!";
+        }
+    }
+    
+    public String deleteGroup(String groupName) {
+        if(groupRepository.existsById(groupName)){
+            groupRepository.deleteById(groupName);
+            return "Group \"" + groupName + "\" has been deleted.";
+        }
+        return "The group \"" + groupName + "\" does not exist, enter a valid group name.";
+    }
+    
+    public String editGroup(String groupNameNow, String groupNameNew) throws Exception {
+        //TODO
+        createGroup(groupNameNew);
+        deleteGroup(groupNameNow);
+
+        return "Group \"" + groupNameNow + "\" has been changed to \"" + groupNameNew + "\"";
+    }
 }
