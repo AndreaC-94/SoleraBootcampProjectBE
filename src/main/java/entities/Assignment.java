b4c40078832848770b46816033b2f9f2a0745bd7
Andrea Colucci <Andrea.Colucci@solera.com>
2022-11-16 15:46:24 +0100

    refactor entitites to entities

--- src/main/java/entities/Assignment.java
+++ src/main/java/entities/Assignment.java
@@ -1,4 +1,4 @@
-package entitites;
+package entities;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
--- src/main/java/entities/Group.java
+++ src/main/java/entities/Group.java
@@ -1,4 +1,4 @@
-package entitites;
+package entities;
 
 
 import java.util.ArrayList;
